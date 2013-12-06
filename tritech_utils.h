da3db6373849196da343735cf16e48327c6251a6
Jody Bruchon <jody@c02ware.com>
2013-12-06 09:47:19 -0500

    Bump version to 1.5

--- tritech_utils.h
+++ tritech_utils.h
@@ -3,6 +3,6 @@
  * existence. For now, we only need this to contain version information.
  */
 
-#define TRITECH_UTILS_VER "1.4"
-#define TRITECH_UTILS_DATE "2013-12-04"
+#define TRITECH_UTILS_VER "1.5"
+#define TRITECH_UTILS_DATE "2013-12-06"
 
