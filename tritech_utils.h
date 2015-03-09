33b6f1ee554a35b483342f4a6fb62d3fc62962d2
Jody Bruchon <jody@c02ware.com>
2015-03-09 16:26:52 -0400

    Minor bug fixes. Bump version to v1.10

--- tritech_utils.h
+++ tritech_utils.h
@@ -3,6 +3,6 @@
  * existence. For now, we only need this to contain version information.
  */
 
-#define TRITECH_UTILS_VER "1.9"
-#define TRITECH_UTILS_DATE "2014-06-16"
+#define TRITECH_UTILS_VER "1.10"
+#define TRITECH_UTILS_DATE "2015-03-09"
 
