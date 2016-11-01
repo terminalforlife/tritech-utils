a9a0358a50ed6a533a14d196bd0e6cb0603f6bee
Jody Bruchon <jody@c02ware.com>
2016-11-01 11:03:11 -0400

    suggest_decompressor: minor code improvements

--- suggest_decompressor.c
+++ suggest_decompressor.c
@@ -7,48 +7,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "tritech_utils.h"
+
+
+/* Extension-to-decompressor table */
+struct ext_t {
+	const char *ext;
+	const char *decomp;
+};
+
+static const struct ext_t ext_table[] = {
+	{ "lzo", "lzop" },
+	{ "tlz", "lzop" },
+	{ "tlzo", "lzop" },
+	{ "7z", "7za" },
+	{ "xz", "xz" },
+	{ "txz", "xz" },
+	{ "lzm", "xz" },
+	{ "lzma", "xz" },
+	{ "gz", "gzip" },
+	{ "tgz", "gzip" },
+	{ "bz2", "bzip2" },
+	{ "tbz", "bzip2" },
+	{ "tbz2", "bzip2" },
+	{ "tz", "gzip" },
+	{ "Z", "gzip" },
+	{ "z", "gzip" },
+	{ "lz", "lzip" },
+	{ "lzip", "lzip" },
+	{ "lz4", "lz4" },
+	{ "lzjody", "lzjody" },
+	{ NULL, 0 }
+};
+
 
 int main(int argc, char **argv)
 {
-	char *ext;
-	struct ext_t {
-		const char *ext;
-		const char *decomp;
-	};
-	struct ext_t ext_table[] = {
-		{ "lzo", "lzop" },
-		{ "tlz", "lzop" },
-		{ "tlzo", "lzop" },
-		{ "7z", "7za" },
-		{ "xz", "xz" },
-		{ "txz", "xz" },
-		{ "lzm", "xz" },
-		{ "lzma", "xz" },
-		{ "gz", "gzip" },
-		{ "tgz", "gzip" },
-		{ "bz2", "bzip2" },
-		{ "tbz", "bzip2" },
-		{ "tbz2", "bzip2" },
-		{ "tz", "gzip" },
-		{ "Z", "gzip" },
-		{ "z", "gzip" },
-		{ "lz", "lzip" },
-		{ "lzip", "lzip" },
-		{ "lz4", "lz4" },
-		{ NULL, 0 }
-	};
-	struct ext_t *p;
+	static const char * restrict ext;
+	static const struct ext_t * restrict p;
 
-	if(argc < 2) {
+	if (argc < 2) {
 		fprintf(stderr, "Tritech compression extension helper %s (%s)", TRITECH_UTILS_VER, TRITECH_UTILS_DATE);
 		fprintf(stderr, "Suggests a decompressor for a given file name\n");
 		fprintf(stderr, "Usage: %s compressed_file.ext\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 	ext = strrchr(argv[1], '.');
-	if(!ext) {
+	if (!ext) {
 		fprintf(stderr, "File name does not have an extension.\n");
 		return EXIT_FAILURE;
 	}
