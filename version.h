c9f21a7552688183914c15b93166ec5f875bf030
Jody Bruchon <jody@jodybruchon.com>
2017-04-30 20:29:17 -0400

    tt_sysupdate: skip already-installed packages, add help text

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
 /* tritech-utils version information */
 
-#define TRITECH_UTILS_VER "1.13"
-#define TRITECH_UTILS_DATE "2016-11-01"
+#define TRITECH_UTILS_VER "1.14"
+#define TRITECH_UTILS_DATE "2017-04-30"
