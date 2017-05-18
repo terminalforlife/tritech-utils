9e81816ae3ca7c14e8a46a2849cbf240c894faf7
Jody Bruchon <jody@jodybruchon.com>
2017-05-18 15:36:55 -0400

    tt_backup_cust_data: work on devices with a 'p' partition

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
 /* tritech-utils version information */
 
-#define TRITECH_UTILS_VER "1.14"
-#define TRITECH_UTILS_DATE "2017-04-30"
+#define TRITECH_UTILS_VER "1.15.1"
+#define TRITECH_UTILS_DATE "2017-05-18"
