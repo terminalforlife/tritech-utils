004048de478edfc4b024051effd4b86649364dc8
Jody Bruchon <jody@jodybruchon.com>
2017-05-30 11:31:05 -0400

    Bump version to 1.15.2

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
 /* tritech-utils version information */
 
-#define TRITECH_UTILS_VER "1.15.1"
-#define TRITECH_UTILS_DATE "2017-05-18"
+#define TRITECH_UTILS_VER "1.15.2"
+#define TRITECH_UTILS_DATE "2017-05-30"
