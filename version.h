8a15536fb01b5f8e6cfe53d80e287b11f32518dd
Jody Bruchon <jody@jodybruchon.com>
2017-06-07 10:08:55 -0400

    tt_cleaner: add a Windows memory dump definition

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
 /* tritech-utils version information */
 
-#define TRITECH_UTILS_VER "1.15.2"
-#define TRITECH_UTILS_DATE "2017-05-30"
+#define TRITECH_UTILS_VER "1.15.3"
+#define TRITECH_UTILS_DATE "2017-06-07"
