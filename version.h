c41f29f12ca1593ce634631f06324d31db55e44c
Jody Bruchon <jody@jodybruchon.com>
2017-08-20 16:20:39 -0400

    v1.15.4

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
 /* tritech-utils version information */
 
-#define TRITECH_UTILS_VER "1.15.3"
-#define TRITECH_UTILS_DATE "2017-06-07"
+#define TRITECH_UTILS_VER "1.15.4"
+#define TRITECH_UTILS_DATE "2017-08-20"
