a16cd23578540c94a9cd4d9e54ac082e766c9c50
Jody Bruchon <jody@jodybruchon.com>
2019-10-30 12:20:37 -0400

    v1.15.7

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
 /* tritech-utils version information */
 
-#define TRITECH_UTILS_VER "1.15.6"
-#define TRITECH_UTILS_DATE "2018-01-25"
+#define TRITECH_UTILS_VER "1.15.7"
+#define TRITECH_UTILS_DATE "2019-10-30"
