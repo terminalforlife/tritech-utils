72e894dab6ddb54dfefdf8e399802a6f574aaef7
Jody Bruchon <jody@jodybruchon.com>
2021-03-24 11:43:44 -0400

    v1.15.8 release

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
 /* tritech-utils version information */
 
-#define TRITECH_UTILS_VER "1.15.7"
-#define TRITECH_UTILS_DATE "2019-10-30"
+#define TRITECH_UTILS_VER "1.15.8"
+#define TRITECH_UTILS_DATE "2021-03-24"
